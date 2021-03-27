30ff91d17e5fe05fa925c5bfcdca38ae6d114606
mtergel <tergelm@gmail.com>
2021-03-27 11:30:34 +0800

    forgot password screen

---
+++ lib/screens/forgot_password/widgets/forgot_password_body.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
+import 'package:shop_app/widgets/custom_suffix_icon.dart';
+import 'package:shop_app/widgets/default_button.dart';
+import 'package:shop_app/widgets/form_error.dart';
+import 'package:shop_app/widgets/no_account_text.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.04),
+              Text('Forgot Password',
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(28),
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const Text(
+                'Please enter your email and we will send \nyou a link to return to your account',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              ForgotPasswordForm()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordForm extends StatefulWidget {
+  @override
+  _ForgotPasswordFormState createState() => _ForgotPasswordFormState();
+}
+
+class _ForgotPasswordFormState extends State<ForgotPasswordForm> {
+  final _formKey = GlobalKey<FormState>();
+  String email;
+  List<String> errors = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (newValue) => email = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.remove(kEmailNullError);
+                });
+              } else if (emailValidatorRegExp.hasMatch(value) &&
+                  errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.remove(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            validator: (value) {
+              if (value.isEmpty && !errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.add(kEmailNullError);
+                });
+              } else if (!emailValidatorRegExp.hasMatch(value) &&
+                  !errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.add(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(color: kPrimaryColor),
+                hintText: 'Enter your email',
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                suffixIcon: CustomSuffixIcon(
+                  svgIcon: 'assets/icons/Mail.svg',
+                )),
+          ),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          FormError(errors: errors),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          DefaultButton(
+            text: 'Continue',
+            press: () {
+              if (_formKey.currentState.validate()) {
+                // no error's go handleSubmit
+              }
+            },
+          ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          const NoAccountText(),
+        ],
+      ),
+    );
+  }
+}
